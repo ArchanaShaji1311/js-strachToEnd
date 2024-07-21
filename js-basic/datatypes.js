61fe4c014adc6d06237b5cc4949a199851452174
ArchanaShaji1311 <134213679+ArchanaShaji1311@users.noreply.github.com>
2024-07-21 18:21:23 +0000

    datatype summary

--- js-basic/datatypes.js
+++ js-basic/datatypes.js
@@ -11,3 +11,22 @@
 console.log(typeof "Archana");
 // typeof(undefined) --> undefined 
 //typeof(null) --> object
+// typeof string --> string
+// typeof number --> number 
+//typeof bigInt --> undefined
+//typeof array --> object
+// type of object --> object
+//typeof function --> object
+
+let fruits = ["apple", "banana","orange"]
+console.log(typeof fruits)
+
+let user = {
+    num : 123,
+    name :"Archana",
+    branch :'CS'
+}
+
+console.log(typeof user)
+
+
