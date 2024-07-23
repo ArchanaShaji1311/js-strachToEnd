f5b67f2215749df6d844d1df48940ade7ae72255
ArchanaShaji1311 <134213679+ArchanaShaji1311@users.noreply.github.com>
2024-07-23 10:21:10 +0000

    modified

--- js-basic/date-time.js
+++ js-basic/date-time.js
@@ -1,11 +1,11 @@
 // Dates
 let myDate = new Date()
-// console.log(myDate)
-// console.log(myDate.toString())
-// console.log(myDate.toDateString())
-// console.log(myDate.toTimeString())
-// console.log(myDate.toLocaleString())
-// console.log(typeof myDate)
+console.log(myDate)
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toTimeString())
+console.log(myDate.toLocaleString())
+console.log(typeof myDate)
 
 // let createdDate = new Date(2023 ,10 ,13)
 // console.log(createdDate.toDateString())
@@ -20,7 +20,7 @@ let createdDate = new Date("07-23-2024")
 // console.log(createdDate.toLocaleString())
 
 let myTimeStamp = Date.now()
-// console.log(myTimeStamp)
-// console.log(createdDate.getTime())
+console.log(myTimeStamp)
+console.log(createdDate.getTime())
 console.log(Math.floor(myTimeStamp/1000))
 // in this way above time in converted to seconds
