ae4541977825e031509be08240fa946f1fd8c38a
ArchanaShaji1311 <134213679+ArchanaShaji1311@users.noreply.github.com>
2024-07-23 18:54:20 +0000

    modified

--- js-basic-2/object-2.js
+++ js-basic-2/object-2.js
@@ -1,38 +1,47 @@
-const instaUser = new Object()
-console.log(instaUser)
+// const instaUser = new Object()
+// console.log(instaUser)
 // the above defined method is example of singleton object
 
-instaUser.id = "abc@1311"
-instaUser.name = "alexia"
-instaUser.isLoggedIn = false
-console.log(instaUser)
+// instaUser.id = "abc@1311"
+// instaUser.name = "alexia"
+// instaUser.isLoggedIn = false
+// console.log(instaUser)
 
 //Nested Objects
-const regularUser = {
-    fullname: {
-        userfullname:{
-            firstname:"Archana",
-            lastname : "Shaji"
-        }
+// const regularUser = {
+//     fullname: {
+//         userfullname:{
+//             firstname:"Archana",
+//             lastname : "Shaji"
+//         }
 
-    }
+//     }
     
-}
-console.log(regularUser.fullname.userfullname.firstname)
+// }
+// console.log(regularUser.fullname.userfullname.firstname)
 
 // combining objects
-const obj1 ={1:"a",2:"b"}
-const obj2={7:"c",8:"d"}
- const obj3 = Object.assign({} , obj1,obj2)
- console.log(obj3)
+// const obj1 ={1:"a",2:"b"}
+// const obj2={7:"c",8:"d"}
+//  const obj3 = Object.assign({} , obj1,obj2)
+//  console.log(obj3)
 
 //another way is spreading
- const obj4 = {...obj1,...obj2}
- console.log(obj4)
+//  const obj4 = {...obj1,...obj2}
+//  console.log(obj4)
 
 // some others methods
-console.log(Object.keys(instaUser))
-console.log(Object.values(instaUser))
-console.log(Object.entries(instaUser))
-console.log(Object.getOwnPropertyNames(instaUser))
-console.log(instaUser.hasOwnProperty("isLoggedIn"))
+// console.log(Object.keys(instaUser))
+// console.log(Object.values(instaUser))
+// console.log(Object.entries(instaUser))
+// console.log(Object.getOwnPropertyNames(instaUser))
+// console.log(instaUser.hasOwnProperty("isLoggedIn"))
+
+const foodDel = {
+    deliverID : "123@123",
+    deliverName :"Alex"
+}
+//destructoring
+const {deliverName:name} = foodDel
+console.log(name)
+
