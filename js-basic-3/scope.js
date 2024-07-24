6ddf81200241b42af1428cb994d2f45c8990c6b4
ArchanaShaji1311 <134213679+ArchanaShaji1311@users.noreply.github.com>
2024-07-24 09:58:42 +0000

    scopes in js

---
+++ js-basic-3/scope.js
@@ -0,0 +1,15 @@
+//var a = 500;
+let b = 100;
+const c = 200;
+if(true)
+{
+    //var a = 700
+    let b = 70
+    const c = 50
+    console.log("Inner b:" , b)
+    console.log("Inner c :" , c)
+}
+// console.log(a)
+console.log(b)
+console.log(c)
+// var has a global scope where as let and const has local scope
