e41f3eaa3765d03b5e62aac531b17ea8b13fd36a
ArchanaShaji1311 <134213679+ArchanaShaji1311@users.noreply.github.com>
2024-07-24 19:21:11 +0000

    if and else and if-else if

---
+++ js-control-flow/01-control.js
@@ -0,0 +1,21 @@
+//if , if-else , if-else if
+//multiple conditional operator work with control flow statements like:
+// < , > , <=, >=, !=, ==, === , !== , && , ||
+
+// basic functioning
+// const age = 21
+// if(age<=18)
+// {
+//     console.log("can't vote")
+// }
+// else{
+//     console.log("can vote")
+// }
+
+const userLoggedId = true
+const debitCard = true
+
+if(userLoggedId && debitCard && 2==2)
+{
+    console.log("Allow to buy course")
+}
