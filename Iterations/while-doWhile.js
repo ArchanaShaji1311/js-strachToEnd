473e7e88e9e74a5de941dbb2b71d8be31a6e95ea
ArchanaShaji1311 <134213679+ArchanaShaji1311@users.noreply.github.com>
2024-07-25 09:25:13 +0000

    while and do-while loop

---
+++ Iterations/while-doWhile.js
@@ -0,0 +1,16 @@
+// while loop
+let num = 1
+while(num<10)
+{
+    console.log(num)
+    num++
+}
+
+// Do-while loop
+let num1 = 1
+do{
+    console.log(`num is : ${num1}`)
+    num1++
+}
+while(num1<=10)
+
